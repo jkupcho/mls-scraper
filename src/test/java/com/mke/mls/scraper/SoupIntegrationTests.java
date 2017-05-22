f9484896d0708ecb2bebfe37f8b904a794a2d1ff
jkupcho <jonathan.kupcho@gmail.com>
2017-05-21 22:38:20 -0500

    started parsing detail page, need to extract into classes.

--- src/test/java/com/mke/mls/scraper/SoupIntegrationTests.java
+++ src/test/java/com/mke/mls/scraper/SoupIntegrationTests.java
@@ -1,16 +1,24 @@
 package com.mke.mls.scraper;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.CaseFormat;
+import com.google.common.base.CharMatcher;
 import com.mke.mls.scraper.domain.MlsListing;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.util.AbstractMap;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
@@ -20,7 +28,7 @@ public class SoupIntegrationTests {
     private static File detailResponse;
     private static File pageResponse;
 
-    private static final String LISTINGS_SITE = "http://mymobile.flexmls.com/donnaschoessow/contacts/20170324014231602695000000/newsfeeds/listings";
+    private static final Log logger = LogFactory.getLog(SoupIntegrationTests.class);
 
     private ObjectMapper objectMapper = new ObjectMapper();
 
@@ -51,7 +59,58 @@ public class SoupIntegrationTests {
 
     @Test
     public void parseDetailPage() throws Exception {
-        Jsoup.parse(detailResponse, "UTF-8");
+        List<Map> listingDetailsMaps = Jsoup.parse(detailResponse, "UTF-8")
+            .select(".detail-fields-group")
+            .stream()
+            .map((group) -> {
+                final Map<String, Object> listingDetails = new HashMap<>();
+                final String key = getLowerCaseKey(group.text());
+
+                Map<String, String> detailEntries = group.nextElementSibling()
+                        .select(".detail-field-wrapper")
+                        .stream()
+                        .filter((wrapper) -> wrapper.children().select(".detail-fields-key").first() != null)
+                        .map((wrapper) -> {
+                            String wrapperKey = getLowerCaseKey(wrapper
+                                    .select(".detail-fields-key")
+                                    .text());
+
+                            String value = wrapper.children().select(".detail-fields-value").text();
+                            return new AbstractMap.SimpleEntry<>(wrapperKey, value);
+                        })
+                        .collect(Collectors.toMap((e) -> e.getKey(), (e) -> e.getValue()));
+
+                listingDetails.put(key, detailEntries);
+
+                return listingDetails;
+            })
+            .collect(Collectors.toList());
+
+        assertEquals(26, listingDetailsMaps.size());
+    }
+
+    private String getLowerCaseKey(String sequence) {
+        String retVal = sequence.toLowerCase();
+
+        retVal = CharMatcher
+                .whitespace()
+                .replaceFrom(retVal, "_");
+
+        retVal = CharMatcher
+                .javaLetterOrDigit()
+                .or(CharMatcher.is('_'))
+                .retainFrom(retVal);
+
+        return retVal;
+    }
+
+    @Test
+    public void testString() {
+        String toJoin = "#Status Change Info".toLowerCase();
+
+        toJoin = getLowerCaseKey(toJoin);
+
+        assertEquals("status_change_info", toJoin);
     }
 
 }
