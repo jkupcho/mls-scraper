81bfdd2b045bc71047caff60c7df56c0246fd67e
jkupcho <jonathan.kupcho@gmail.com>
2017-05-21 20:35:26 -0500

    removed actual page requests from the junits to avoid hitting the server.

--- src/test/java/com/mke/mls/scraper/SoupIntegrationTests.java
+++ src/test/java/com/mke/mls/scraper/SoupIntegrationTests.java
@@ -1,34 +1,58 @@
 package com.mke.mls.scraper;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import com.mke.mls.scraper.domain.MlsListing;
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.springframework.core.io.ClassPathResource;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static org.junit.Assert.assertEquals;
+
 public class SoupIntegrationTests {
 
-    private final Log logger = LogFactory.getLog(getClass());
+    private static File detailResponse;
+    private static File pageResponse;
 
-    public static final String LISTINGS_SITE = "http://mymobile.flexmls.com/donnaschoessow/contacts/20170324014231602695000000/newsfeeds/listings";
+    private static final String LISTINGS_SITE = "http://mymobile.flexmls.com/donnaschoessow/contacts/20170324014231602695000000/newsfeeds/listings";
 
     private ObjectMapper objectMapper = new ObjectMapper();
 
+    @BeforeClass
+    public static void setupFiles() throws Exception {
+        detailResponse = new ClassPathResource("html_response.html").getFile();
+        pageResponse = new ClassPathResource("page_request.html").getFile();
+    }
+
     @Test
     public void parsePage() throws Exception {
-
-        List<String> dataListingJson = Jsoup.connect(LISTINGS_SITE)
-                .header("X-Requested-With", "XMLHttpRequest")
-                .get()
+        List<MlsListing> listings = Jsoup.parse(pageResponse, "UTF-8")
                 .select("li.photo-listing-card > div.photo-listing-card-wrapper > a.saveListingLink")
                 .stream()
                 .map((anchor) -> anchor.attr("data-listing"))
+                .map((dataJson) -> {
+                    try {
+                        return objectMapper.readValue(dataJson, MlsListing.class);
+                    } catch (Exception e) {
+                        return null;
+                    }
+                })
+                .filter((result) -> result != null)
                 .collect(Collectors.toList());
 
-//        logger.info(Arrays.toString(dataListingJson.toArray()));
+        assertEquals(10, listings.size());
+    }
+
+    @Test
+    public void parseDetailPage() throws Exception {
+        Jsoup.parse(detailResponse, "UTF-8")
+            ;
     }
 
 }
